f957f02e79c250a0684feb8294f56dd0a56d0bce
Shueb Dalvi <shueb123@gmail.com>
2022-08-13 14:44:20 +0530

    initial commit

---
+++ src/main/java/com/shuebdalvi/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.shuebdalvi.web.rest.vm;
---
+++ src/test/java/com/shuebdalvi/IntegrationTest.java
@@ -0,0 +1,22 @@
+package com.shuebdalvi;
+
+import com.shuebdalvi.TodoappApp;
+import com.shuebdalvi.config.AsyncSyncConfiguration;
+import com.shuebdalvi.config.EmbeddedSQL;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { TodoappApp.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
